e1755414d7701a44499c0cc3b8bb37a8b7f142c8
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2026-05-21 00:51:56 +0700

    feat: add file config

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
@@ -7,5 +8,12 @@ HF_TOKEN = os.getenv("HF_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 CHROMA_DB_PATH = "./chroma_db"
+CHROMA_COLLECTION_NAME = "ragnar"
+
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
+
+UPLOAD_DIR = Path("data/pdf_uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+MAX_FILE_SIZE = 50 * 1_024 * 1_024
